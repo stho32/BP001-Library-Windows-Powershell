4a3a0e0a3601f695dcab69af813433c28c96ff38
Stefan Hoffmann <stho32github@gmail.com>
2022-04-22 17:51:16 +0000

    ensured a valid working directory for the structural setup

--- setup-structure.ps1
+++ setup-structure.ps1
@@ -1,4 +1,11 @@
-﻿$modulename = Read-Host -Prompt "What shall the name of your new module be?"
+﻿<#
+    Sets up the basic project structure
+#>
+
+$ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot
+
+$modulename = Read-Host -Prompt "What shall the name of your new module be?"
 $token = Read-Host -Prompt "Choose a short about-3-letter token that shall represent commands of your module"
 $token = $token.ToUpper()
 
@@ -6,3 +13,5 @@ Write-Host "  - renaming module file"
 Rename-Item .\module\Modulename.ps1 "$modulename.psm1"
 Write-Host "  - renaming module folder"
 Rename-Item .\module $modulename
+
+Pop-Location
