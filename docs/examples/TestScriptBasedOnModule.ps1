f20064816162a15de302c7e09b8dd6e955a7b589
stho32 <stho32github@gmail.com>
2022-05-20 14:07:23 +0200

    Update TestScriptBasedOnModule.ps1

--- docs/examples/TestScriptBasedOnModule.ps1
+++ docs/examples/TestScriptBasedOnModule.ps1
@@ -2,10 +2,12 @@
     This file demonstrates how a script that uses the module can load and reload the module 
     for a fast development cycle which changes the script as well as the module
 #>
-
+Push-Location $PSScriptRoot
 $ErrorActionPreference = "Stop"
 
 Remove-Module Modulename*
 Import-Module ..\module\Modulename.psm1
 
 Write-LogToScreen "Hello World" -Type Warning
+
+Pop-Location
